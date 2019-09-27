e39921423974aee12740a8f7f8c9c5b69ecd47a9
momo13910 <mohamed.yahyaoui10@sfr.fr>
2019-09-27 10:21:29 +0200

    home

--- index.php
+++ index.php
@@ -41,16 +41,27 @@ if ($_POST) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
   <meta charset="UTF-8">
+  <link rel="stylesheet" href="css/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Log In</title>
 </head>
 <body>
-  <form action="" method="post">
-    <input type="text" name="username" placeholder="Pseudo">
-    <input type="password" name="password" placeholder="Password">
-    <input type="submit" value="Send">
-  </form> 
+<div class="body"></div>
+		<div class="grad"></div>
+		<div class="header">
+			<div>Site<span>Quiz</span></div>
+		</div>
+		<br>
+		<div class="login">
+				<input type="text" placeholder="username" name="user"><br>
+				<input type="password" placeholder="password" name="password"><br>
+        <input type="button" value="Login">
+        <div id="register"><p>Not registered yet? </p> <a href="">  Register</a></div>
+        
+		</div>
+ 
 </body>
 </html>
